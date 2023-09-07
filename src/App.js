7c7ce31b5aa36bfccc85a6e0bcaf3bac5823b851
AbhinavPaliwal <91108086+AbhinavPaliwal@users.noreply.github.com>
2023-09-07 15:23:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TicTacToe from "./components/TicTacToe";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h2 className="text-center text-white">Tic-Tac-Toe</h2>
+      <TicTacToe />
+    </div>
+  );
+}
